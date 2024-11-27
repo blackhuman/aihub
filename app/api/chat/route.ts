b9d32dec428faabc61ffb94b0b300ea43896bce4
August White <blackhuman01@gmail.com>
2024-11-27 20:42:57 +0800

    update cors

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,13 +4,15 @@ import { getEdgeRuntimeResponse } from "@assistant-ui/react/edge";
 export const maxDuration = 30;
 
 export const POST = async (request: Request) => {
+  const apiKey = request.headers.get('abc123');
+  if (!apiKey) return new Response("No API key provided", { status: 401 });
   const requestData = await request.json();
 
-  return getEdgeRuntimeResponse({
+  return await getEdgeRuntimeResponse({
     options: {
       model: openai("gpt-4o"),
     },
     requestData,
     abortSignal: request.signal,
-  });
-};
+  })
+}
