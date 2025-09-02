b843bd9212e02946f91a8c8aed4eed361a25eadf
baiyizhe <baiyizhe@meituan.com>
2025-09-02 13:11:42 +0800

    update

--- ai-backend/src/middleware.ts
+++ ai-backend/src/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher, currentUser } from '@clerk/nextjs/server';
+import { clerkClient, clerkMiddleware, createRouteMatcher, currentUser } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 export const config = {
@@ -17,11 +17,22 @@ export default clerkMiddleware(async (auth, req) => {
     if (req.method === 'OPTIONS') {
       return NextResponse.next()
     }
-    const { userId } = await auth()
-    console.log('userId:', userId)
+    const { userId, getToken, has, sessionId } = await auth()
+    const token = await getToken()
     // await auth.protect()
     if (!userId) {
       return NextResponse.json({ error: 'unauthorized' }, { status: 401 })
     }
+
+    // const clerk = await clerkClient()
+    // const user = await currentUser()
+    // console.log('user:', user)
+    // const session = await clerk.sessions.getSession(sessionId)
+    // const token2 = req.headers.get('Authorization')?.replace('Bearer ', '')
+    // const reqState = await clerk.authenticateRequest(req)
+    // console.log('reqState:', reqState)
+    // auth.protect()
   }
+}, {
+  debug: true
 });
